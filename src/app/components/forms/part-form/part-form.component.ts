3ed5155cf58ef8d2afa4d751bd16b475417d984c
Dev <huntinghour@gmail.com>
2020-03-17 18:29:56 +0530

    8

--- src/app/components/forms/part-form/part-form.component.ts
+++ src/app/components/forms/part-form/part-form.component.ts
@@ -191,7 +191,22 @@ export class PartFormComponent implements OnInit {
     this.files.splice(i, 1);
 
   }
-
+  /**
+   * format bytes
+   * @param bytes (File size in bytes)
+   * @param decimals (Decimals point)
+   */
+  formatBytes(bytes, decimals) {
+    if (bytes === 0) {
+      return '0 Bytes';
+    }
+    const k = 1024;
+    const dm = decimals <= 0 ? 0 : decimals || 2;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
+  
   onSubmit() {
     console.log(this.partsInventoryForm);
     let customData = this.customPartForm.value.customDataArray
